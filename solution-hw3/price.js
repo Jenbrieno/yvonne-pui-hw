73e38241249d47c8921b154933ffafdd994c6877
Yvonne Huang <huangq9@uw.edu>
2022-09-23 11:10:20 -0400

    updates on hw3

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -1,7 +1,13 @@
 function glazingChange(element) {
     // get value of selected glazing option
     const priceChange = element.value;
-    
+  
   // add your code to do update the price ...
   }
   
+const noteImageElement = documennt.querySelector('.glazingOptions');
+console.log(noteImageElement);
+
+// const glazing = {
+  
+// }
