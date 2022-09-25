8502a9b217cfb8ce5b6126ba52d3343516217b68
Yvonne Huang <huangq9@uw.edu>
2022-09-25 06:50:51 -0400

    update photo

--- solution-hw3/price.js
+++ solution-hw3/price.js
@@ -1,13 +1,35 @@
-function glazingChange(element) {
-    // get value of selected glazing option
-    const priceChange = element.value;
-  
-  // add your code to do update the price ...
-  }
-  
-const noteImageElement = documennt.querySelector('.glazingOptions');
-console.log(noteImageElement);
 
-// const glazing = {
-  
-// }
+let bun = [
+  {
+    glazing: 'Keep Original',
+    price: 2.49,
+  },
+  {
+    glazing: 'Sugar Milk',
+    price: 2.49,
+  },
+  {
+    glazing: 'Vanilla Milk',
+    price: 2.99,
+  },
+  {
+    glazing: 'Double Chocolate',
+    price: 3.99,
+  },
+];
+
+function displayOptions(){
+  let bunGlazing = document.querySelector('#glazing-options');
+  let bunPackSize = document.querySelector('#pack-size');
+
+  bunGlazing.innerText = bun.glazing;
+  bunPackSize.innerText = bun.size;
+}
+
+function glazingChange(element) {
+  // get value of selected glazing option
+  const priceChange = element.value;
+
+// add your code to do update the price ...
+  console.log('Total: ' + priceChange);
+}
